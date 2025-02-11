14c280fc209131fac3a77124d1b9ae58d67324c9
thomas <thomaspiet@outlook.fr>
2025-02-11 09:15:50 +0100

    vercel Refactor database schema and form API route naming conventions

--- app/api/form/route.js
+++ app/api/form/route.js
@@ -5,27 +5,28 @@ export async function GET() {
   try {
     const form = await db.form.findMany({
       orderBy: {
-        CreatedAt: 'desc'
+        createdAt: 'desc'
       }
     });
     const question = await db.question.findMany();
     const response = await db.response.findMany();
-  
+
     const data = form.map((f) => {
       const q = question.filter((q) => q.formId === f.id);
-  
-      const date = new Date(f.CreatedAt).toLocaleDateString("fr-FR");
-  
+      const date = new Date(f.createdAt).toLocaleDateString("fr-FR");
+
       return {
         name: f.name,
-        CreatedAt: date,
+        createdAt: date,
         questions: q.length,
         reponses: response.filter((r) => r.formId === f.id).length,
         id: f.id,
       };
     });
+
     return NextResponse.json(data);
   } catch (error) {
+    console.error("Erreur dans GET /api/form :", error);
     return NextResponse.json({ error: "Failed to fetch forms" }, { status: 500 });
   }
 }
